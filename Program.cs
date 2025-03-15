9a2c3056aba3ec93e8fc17db2bcc3e4f04dbb233
Tdarrelpa <darrel2208@gmail.com>
2025-03-15 21:04:26 +0700

    menambahkan class DataGeneric

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using CobaDataGeneric;
+public class Program
+{
+    public static void Main()
+    {
+        DataGeneric<int> nilai = new DataGeneric<int>(100);
+        DataGeneric<double> IPK = new DataGeneric<double>(3.6);
+        DataGeneric<string> dataString = new DataGeneric<string>("Darrel");
+        nilai.PrintData();
+        IPK.PrintData();
+        dataString.PrintData();
+    }
+}
